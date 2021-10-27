52f0e72b58353452b7c8ed71e5d4125597a92c13
thewi <thewi@DESKTOP-6MVA50V>
2021-10-27 10:35:46 +0100

    Kata4 Version1

--- src/kata4/main/Kata4.java
+++ src/kata4/main/Kata4.java
@@ -2,6 +2,10 @@
 
 package kata4.main;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -13,18 +17,51 @@ import kata4.view.MailListReader;
 
 
 public class Kata4 {
-
+    public static List<Mail> mails = new ArrayList<Mail>();
+    
     public static void main(String[] args) {
-        String file = "email.txt";
-        MailListReader mailReader = new MailListReader();
-        MailHistogramBuilder histBuilder = new MailHistogramBuilder();
+        execute();
+
+    }
+    
+    public static void execute(){
         
-        List<Mail> mails = new ArrayList<Mail>(mailReader.read(file));
-        System.out.println(Arrays.toString(mails.toArray()));
+        input();
+        proccess();
+        output();
+    }
+    
+    public static void input(){
+        String file = "email.txt";
+        List<Mail> res = new ArrayList<Mail>();
+        try{
+            
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while((line=br.readLine()) != null){
+                Mail lineMail = new Mail(line);
+                res.add(lineMail);
+            }
+            fr.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        }
+    
+    public static void proccess(){
+        MailListReader mailReader = new MailListReader();
+        String file = "email.txt";
+        mails = mailReader.read(file);
+    }
+    
+    public static void output(){
         Histogram histogram = new Histogram();
+        MailHistogramBuilder histBuilder = new MailHistogramBuilder();
         histogram = histBuilder.build(mails);
         new HistogramDisplay(histogram).execute();
-        
-        
     }
+    
 }
